3abb35f0058cc46bfe1c7aae7cb616d3e1e1055d
1kuperster@gmail.com <1kuperster@gmail.com>
2022-08-10 23:12:31 +0300

    кличество

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-import os
-
-## Move into the directory where the database is.
-os.chdir('C:/Users/1kupe/PycharmProjects/pythonProject/')
-
-open_file = open('!_9100-2.txt', 'r', encoding="utf-8")
-line = open_file.readline()
-name = line.strip()
-
-candidateDict = {}
-print(candidateDict)
+import re
 
 
-def find_duplicate():
-    for name in open_file:
-        if name in candidateDict:
-            candidateDict[name] += 1
-        else:
-            candidateDict[name] = 1
+# read a text file
+
+file = open('!_9100-2.txt', 'r', encoding="utf-8")
+text = file.read()
+file.close()
+
+# find a number using regex
+pattern = re.compile(r'[-(]\d+-\d+')
+numbers = pattern.findall(text)
 
 
-find_duplicate()
+# write those numbers into a list
+numbers_list = []
+for number in numbers:
+    numbers_list.append(str(number))
 
-for name in candidateDict:
-    if candidateDict[name] > 1:
-        print(name)
-print(name)
-open_file.close()
+
+def find_repeats(numbers_list):
+    """find repeats in a list and print them"""
+    import collections
+    numbers_dict = collections.Counter(numbers_list)
+    for key, value in numbers_dict.items():
+        if value > 1:
+            print(key, value)
+    return numbers_dict
+
+oak = find_repeats(numbers_list)
+
+'''new file txt to oak.txt'''
+file = open('oak.txt', 'w', encoding="utf-8")
+file.write(str(oak))
+file.close()
+
